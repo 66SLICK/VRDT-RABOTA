9310e09c43289d8cc9152a24af2f74d3770f45f4
66SLICK <135689062+66SLICK@users.noreply.github.com>
2025-01-13 11:10:49 +0300

    Update Program.cs

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    string firstName = "Дмитрий";
+    string lastName = "Титов";
+    Console.Writeline("Имя" + firstName);
+    Console.Writeline("Фамилия" + lastName);
